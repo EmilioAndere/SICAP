774f4bbe88308252628a2bcf079ae9972d6bb0fa
EmilioAndere <emilio.andere.l@gmail.com>
2022-04-13 22:45:11 -0500

    Pantalla de Inicio

--- Views/Home.php
+++ Views/Home.php
@@ -4,10 +4,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./Views/css/home.css">
     <title>CAP - Home</title>
+    <style>
+    </style>
 </head>
 <body>
-    <h1>Welcome CAP</h1>
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background: #01AA9E !important">
+  <div class="container-fluid">
+      <!-- <a class="navbar-brand" href="#"> -->
+        <img src="./Views/img/logo-upt.png" class="" width="70" height="50">
+      <!-- </a> -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/expedientes">Expedientes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pacientes</a>
+        </li>
+      </ul>
+      <span class="navbar-text">
+        <a href="#" class="nav-link">Cerrar Sesion</a>
+      </span>
+    </div>
+  </div>
+</nav>
+<div id="contenedor">
+  <h1 id="title">CAP</h1>
+</div>
 </body>
 <script>
 
--- index.php
+++ index.php
@@ -4,7 +4,6 @@ include_once './Core/Application.php';
 
 $url = $_GET['url'];
 $url = explode("/", $url);
-
 foreach($middle as $middleware => $classes){
     if(in_array(ucfirst($url[0]), $classes)){
         include_once './Middlewares/'.$middleware.".php";
@@ -12,7 +11,8 @@ foreach($middle as $middleware => $classes){
         $instanceMiddle->handle($url);
     }else{
         if(count($url) == 1){
-            require_once './Views/'.ucfirst($url[0]).".php";
+            $title = ucfirst($url[0]);
+            include_once './Views/'.ucfirst($url[0]).".php";
         }else{
             $controller = ucfirst($url[0])."Controller";
             include_once './Controllers/'.$controller.".php";
